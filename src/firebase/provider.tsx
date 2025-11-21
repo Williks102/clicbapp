898ef79af3737d3b83e2951e772f53933866e533
Pauline Lago <lagopauline28@gmail.com>
2025-11-21 17:17:00 +0000

    [Event Page] 🔥 areServicesAvailable: true page.tsx:44 [Event Page] 📋 E

--- src/firebase/provider.tsx
+++ src/firebase/provider.tsx
@@ -113,10 +113,14 @@ export const useFirebaseApp = (): FirebaseApp => {
 type MemoFirebase <T> = T & {__memo?: boolean};
 
 export function useMemoFirebase<T>(factory: () => T, deps: DependencyList): T | (MemoFirebase<T>) {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   const memoized = useMemo(factory, deps);
   
   if(typeof memoized !== 'object' || memoized === null) return memoized;
-  (memoized as MemoFirebase<T>).__memo = true;
+  
+  try {
+    (memoized as MemoFirebase<T>).__memo = true;
+  } catch {}
   
   return memoized;
 }
