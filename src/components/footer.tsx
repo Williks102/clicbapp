133d81cedd5f887456cd2bac118fa1e720284516
Pauline Lago <lagopauline28@gmail.com>
2025-12-04 11:33:54 +0000

    ajoute ces 3 pages dans le footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -46,13 +46,13 @@ const Footer = () => {
                 </Link>
               </li>
               <li>
-                <Link href="#" className="hover:text-primary">
-                  FAQ
+                <Link href="/contact" className="hover:text-primary">
+                  Contact & Support
                 </Link>
               </li>
               <li>
                 <Link href="#" className="hover:text-primary">
-                  Support
+                  FAQ
                 </Link>
               </li>
             </ul>
