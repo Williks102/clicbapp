81b8d51dd401a73f7a4390f9701a0fe2d0bd062e
Pauline Lago <lagopauline28@gmail.com>
2026-01-18 19:24:36 +0000

    Comment résoudre ça ?

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,20 @@
+
 import type { NextConfig } from 'next';
+
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://widget.cloudinary.com;
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+    img-src 'self' blob: data: https://res.cloudinary.com https://images.unsplash.com https://placehold.co https://picsum.photos https://firebasestorage.googleapis.com https://storage.googleapis.com https://api.qrserver.com https://lh3.googleusercontent.com;
+    font-src 'self' https://fonts.gstatic.com;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    connect-src 'self' https://*.googleapis.com https://*.firebaseapp.com https://vitals.vercel-insights.com https://api.cloudinary.com;
+    frame-src 'self' https://widget.cloudinary.com;
+    upgrade-insecure-requests;
+`.replace(/\s{2,}/g, ' ').trim();
 
 const nextConfig: NextConfig = {
   /* TypeScript Configuration */
@@ -9,6 +25,20 @@ const nextConfig: NextConfig = {
   /* ESLint Configuration */
   eslint: {
     ignoreDuringBuilds: true,
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader,
+          },
+        ],
+      },
+    ];
   },
 
   /* Images Configuration */
